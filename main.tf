bcf6884591506c5c8fa3149f498b3d19f6d10a10
Sulamy <sulamy.almeida.ts@gmail.com>
2025-02-11 21:28:53 -0300

    criando Glue Bronze

--- main.tf
+++ main.tf
@@ -46,3 +46,24 @@ resource "aws_iam_role_policy_attachment" "glue_s3_access" {
   role       = aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
+
+# 🚀 Criar o Glue Job e apontá-lo para o script no S3
+resource "aws_glue_job" "glue_job" {
+  name     = var.glue_job_cadastro
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    script_location = "s3://${var.s3_bucket_name}/${var.glue_job_cadastro}"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--TempDir"       = var.s3_output_path
+    "--job-language"  = "python"
+    "--enable-continuous-cloudwatch-log" = "true"
+  }
+
+  glue_version = "3.0"
+  worker_type  = "G.1X"
+  number_of_workers = 2
+}
--- variables.tf
+++ variables.tf
@@ -23,3 +23,27 @@ variable "glue_role_name" {
   type        = string
   default     = "GlueServiceRole"
 }
+
+variable "glue_job_cadastro" {
+  description = "Nome do AWS Glue Job"
+  type        = string
+  default     = "tb_bronze_cadastro_usuarios_2"
+}
+
+variable "s3_script_path" {
+  description = "Caminho do script PySpark no S3"
+  type        = string
+  default     = "s3://dl-script/tb_bronze_cadastro_usuarios_2.py"
+}
+
+variable "s3_bucket_name" {
+  description = "Nome do bucket S3 onde armazenamos o script"
+  type        = string
+  default     = "dl-script"
+}
+
+variable "s3_output_path" {
+  description = "Caminho para salvar os resultados no S3"
+  type        = string
+  default     = "s3://dl-resultado-glue-output/"
+}
